ee1efcbc3c4fb82e97a22ec27750cdd2b98ddc77
jakkarin <jakkarin@buu.ac.th>
2023-01-03 16:21:45 +0700

    Project Template Example

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExampleApp());
+}
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DevicePreview(
+      tools: const [
+        DeviceSection(),
+      ],
+      builder: (context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        useInheritedMediaQuery: true,
+        builder: DevicePreview.appBuilder,
+        locale: DevicePreview.locale(context),
+        title: 'Responsive and adaptive UI in Flutter',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text('App Title'),
+          ),
+          body: Container(),
+        ),
+      ),
+    );
+  }
+}
